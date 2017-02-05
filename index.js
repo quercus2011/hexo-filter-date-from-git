61aa2b5184b159029cd6acc20b4d03399c32715f
Quercus Maris <quercus2011@gamil.com>
2017-02-05 14:48:52 +0900

    Refactoring

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const moment = require('moment-timezone');
 
 hexo.extend.filter.register('before_post_render', data => {
   const originDate = data.date;
-  const gitDate = getDate(data);
+  const gitDate = getDateOfOldestGitLog(data, '');
   if (gitDate && gitDate < originDate) {
     data.date = gitDate;
   }
 
   const originUpdated = data.updated;
-  const gitUpdated = getUpdated(data);
+  const gitUpdated = getDateOfOldestGitLog(data, '-1');
   if (gitUpdated && gitUpdated < originUpdated) {
     data.updated = gitUpdated;
   }
@@ -22,24 +22,15 @@ hexo.extend.filter.register('before_post_render', data => {
   return data;
 });
 
-function getDate(data) {
+function getDateOfOldestGitLog(data, opt) {
   const filePath = getFilePath(data);
-  const log = execSync(`git log --follow --format="%ad" -- ${filePath}`).toString().trim();
+  const log = execSync(`git log --follow ${opt} --format="%ad" -- ${filePath}`).toString().trim();
   const date = log.slice(log.lastIndexOf('\n') + 1);
   // If the file is created a moment ago, it will be an untracked file, then git can not log it
   if (date === '') {
     return undefined;
   }
   return moment(new Date(date));
-}
-
-function getUpdated(data) {
-  const filePath = getFilePath(data);
-  const updated = execSync(`git log --follow -1 --format="%ad" -- ${filePath}`).toString().trim();
-  if (updated === '') {
-    return undefined;
-  }
-  return moment(new Date(updated));
 }
 
 function getFilePath(data) {
