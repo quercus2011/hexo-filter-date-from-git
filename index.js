f47966e88b3c919a764ed8e174074c3595dd52ae
Quercus Maris <quercus2011@gmail.com>
2017-03-08 20:13:49 +0900

    Rewrite wholly, and adopt "js-yaml" package instead of "yaml-front-matter"

--- index.js
+++ index.js
@@ -2,45 +2,88 @@
 /* eslint no-param-reassign:0, strict:0, max-len:0 */
 'use strict';
 
-const yaml = require('yaml-front-matter');
+const assert = require('assert');
+const jsYaml = require('js-yaml');
 const execSync = require('child_process').execSync;
 const moment = require('moment-timezone');
 
+const rFrontMatter = /^-{3,}[\r\n](?:((\s*(\S)[^]*?(\S))?\s*)[\r\n])?-{3,}[\r\n]/u;
+
+function parseFrontMatter(text, filePath) {
+  const matching = rFrontMatter.exec(text);
+  if (! matching) throw new SyntaxError(`Invalid front-matter: ${filePath}`);
+  if (! matching[2]) return {};
+  if (matching[3] === '{') {
+    if (matching[4] !== '}') throw new SyntaxError(`Invalid JSON front-matter: ${filePath}`);
+    try {
+      return JSON.parse(matching[1]);
+    } catch (err) {
+      hexo.log.debug(err);
+      throw new SyntaxError(`Invalid JSON front-matter: ${filePath}`);
+    }
+  } else {
+    try {
+      return jsYaml.load(matching[1], {
+        filename: filePath,
+        schema: jsYaml.CORE_SCHEMA
+      });
+    } catch (err) {
+      hexo.log.debug(err);
+      throw new SyntaxError(`Invalid YAML front-matter: ${filePath}`);
+    }
+  }
+}
+
+function getGitLogs(filePath, logger) {
+  // If the file is created a moment ago, it will be an untracked file, then git can not log it
+  try {
+    const list = execSync(`git log --follow --format="%ad" -- ${filePath}`).toString().trim().split(/\r?\n/);
+    list.pop();
+    const gitLogOldest = list.pop();   // may be undefined
+    const gitLogNewest = list.shift(); // may be undefined
+    return {
+      date:    gitLogOldest,
+      updated: gitLogNewest
+    };
+  } catch (err) {
+    logger(`"git log" failed for ${filePath}: ${err}`);
+    return {};
+  }
+}
+
+function selectTimestamp(name, data, frontMatter, gitLogs, filePath, timezone, logger) {
+  const org = data[name];
+  const fm = frontMatter[name];
+  const git = gitLogs[name];
+  assert(org, `BUG: data.${name} should be set: ${filePath}`);
+  if (fm) {
+    const result = moment.tz(fm, timezone);
+    if (! result || ! result.isValid()) throw new Error(`Invalid timestamp "${fm}" in front-matter: ${filePath}`);
+    assert(result.isSame(org), `BUG: data.${name} should be equal to "${name}" of front-matter: ${filePath}`);
+    return result;
+  } else if (git) {
+    const result = moment.tz(git, timezone);
+    if (! result || ! result.isValid()) throw new Error(`Invalid timestamp "${git}" in git log: ${filePath}`);
+    return result;
+  } else {
+    assert(org.isValid(), `BUG: Invalid data.${name}: "${org.format()}" for ${filePath}`);
+    return org;
+  }
+}
+
 hexo.extend.filter.register('before_post_render', data => {
-  const frontMatter = yaml.loadFront(data.raw);
+  const logger = hexo.log.debug.bind(hexo.log);
+  const timezone = hexo.config.timezone;
   const filePath = data.full_source;
+  const frontMatter = parseFrontMatter(data.raw, filePath);
+  const gitLogs = getGitLogs(filePath, logger);
 
-  if (! frontMatter.date) {
-    const gitDate = getDateOfOldestGitLog(filePath, '');
-    if (gitDate) {
-      data.date = gitDate;
-    }
-  }
+  if (! moment.tz.zone(timezone)) throw new Error(`Invalid "timezone" in hexo configuration: "${timezone}"`);
 
-  if (! frontMatter.updated) {
-    const gitUpdated = getDateOfOldestGitLog(filePath, '-1');
-    if (gitUpdated) {
-      data.updated = gitUpdated;
-    }
-  }
+  data.date = selectTimestamp('date', data, frontMatter, gitLogs, filePath, timezone, logger);
+  data.updated = selectTimestamp('updated', data, frontMatter, gitLogs, filePath, timezone, logger);
+
+  if (data.date.isAfter(data.updated)) hexo.log.warn(`"data.date" contradicts "data.updated": ${filePath}`);
 
   return data;
 });
-
-function execSyncCasually(command) {
-  try {
-    return execSync(command);
-  } catch (err) {
-    return '';
-  }
-}
-
-function getDateOfOldestGitLog(filePath, opt) {
-  const log = execSyncCasually(`git log --follow ${opt} --format="%ad" -- ${filePath}`).toString().trim();
-  const date = log.slice(log.lastIndexOf('\n') + 1);
-  // If the file is created a moment ago, it will be an untracked file, then git can not log it
-  if (date === '') {
-    return undefined;
-  }
-  return moment(new Date(date));
-}
